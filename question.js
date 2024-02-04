752c848de0dca6eb44bd4604d584e3d50c5fcc5f
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2024-02-04 23:13:33 +0530

    Update question.js

--- question.js
+++ question.js
@@ -362,3 +362,17 @@ console.log(unboundGetX());  // Output: undefined
 
 const boundGetX = unboundGetX.bind(obj);
 console.log(boundGetX());  // Output: 42
+
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
